2936f85d2e15de86b3189f80dfae05d0071bca8b
Kristijan Boskovic <kristijan00b@gmail.com>
2026-01-14 21:46:45 +0100

    Implement event details

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
       >
     <Navbar />
-      <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+      <div className="fixed inset-0 top-0 z-[-1] min-h-screen" style={{ position: "fixed" }}>
         <LightRays
               raysOrigin="top-center-offset"
               raysColor="#5dfeca"
--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -14,17 +14,17 @@ const EventCard = ({title,image,slug,location,date,time}:Props) => {
         <Link href={`/events/${slug}`} id="event-card">
             <Image src={image} alt={title} width={410} height={300} className="poster"/>
             <div className="datetime">
-                <Image src="icons/pin.svg" alt="location" width={14} height={14}/>
+                <Image src="/icons/pin.svg" alt="location" width={14} height={14}/>
                 <p>{location}</p>
             </div>
             <p className="title">{title}</p>
             <div className="datetime">
                 <div>
-                    <Image src="icons/calendar.svg" alt="date" width={14} height={14}/>
+                    <Image src="/icons/calendar.svg" alt="date" width={14} height={14}/>
                     <p>{date}</p>
                 </div>
                 <div>
-                    <Image src="icons/clock.svg" alt="time" width={14} height={14}/>
+                    <Image src="/icons/clock.svg" alt="time" width={14} height={14}/>
                     <p>{time}</p>
                 </div>
             </div>
